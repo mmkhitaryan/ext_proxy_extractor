d4c7fb82b429bac97ffc541502160896b8557609
Marat Mkhitaryan <marat@mkhitaryan.pw>
2023-05-27 03:03:32 +0400

    refactor

--- extractors/base.py
+++ extractors/base.py
@@ -1,5 +1,8 @@
 import requests
 
+# TODO: move request+proxy extraction as strategy pattern
+# in most of the extractors we only have request and json parsing logic
+# to avoid boilerplating this logic should be placed in strategy class
 class BaseExtractor():
     requests_instance = requests.Session()
 
